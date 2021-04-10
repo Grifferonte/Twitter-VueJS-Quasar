de69d6ff6df56009b26ed2b3509db693b0887bc6
Grifferonte <lucas.risi93@gmail.com>
2021-04-10 22:24:12 +0200

    Update routes

--- qTwitter/src/router/routes.js
+++ qTwitter/src/router/routes.js
@@ -1,19 +1,27 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { 
+        path: "", 
+        component: () => import("pages/PageHome.vue"),
+        name:'Home', 
+      },
+      { 
+        path: "/about", 
+        component: () => import("pages/PageAbout.vue"),
+        name:'About', 
+      }
     ]
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '*',
-    component: () => import('pages/Error404.vue')
+    path: "*",
+    component: () => import("pages/Error404.vue")
   }
-]
+];
 
-export default routes
+export default routes;
