571c6a49913102bbce65133ed7b79d7230931356
Grifferonte <lucas.risi93@gmail.com>
2021-05-04 21:27:16 +0200

    Fix App

--- qTwitter/src/layouts/MainLayout.vue
+++ qTwitter/src/layouts/MainLayout.vue
@@ -83,7 +83,9 @@
     </q-drawer>
 
     <q-page-container>
-      <router-view />
+      <keep-alive>
+        <router-view />
+      </keep-alive>
     </q-page-container>
   </q-layout>
 </template>
